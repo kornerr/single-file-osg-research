12519420027f35fc86d721620c48db80508cbb07
Michael Kapelko <kornerr@gmail.com>
2019-02-14 12:28:43 +0300

    Add OpenThreads/Affinity header

--- OpenSceneGraph.h
+++ OpenSceneGraph.h
@@ -380,6 +380,51 @@ private:
 }
 
 
+// OSGFILE include/OpenThreads/Affinity
+
+#include <set>
+//#include <OpenThreads/Mutex>
+
+namespace OpenThreads {
+
+/**
+ *  @class Affinity
+ *  @brief  Simple container for specifying which CPU a thread should have affinity with.
+ *  An empty Affinity.activeCPUs/default constructed Affinity signifies that a thread should not have any specific affinity and be able to run on all available CPUs.
+ */
+class Affinity
+{
+public:
+
+    Affinity() {}
+
+    Affinity(unsigned int cpuNumber) { activeCPUs.insert(cpuNumber); }
+
+    Affinity(unsigned int cpuNumber, unsigned int cpuCount) { while(cpuCount>0) { activeCPUs.insert(cpuNumber++); --cpuCount; } }
+
+    Affinity(const Affinity& rhs) : activeCPUs(rhs.activeCPUs) {}
+
+    Affinity& operator = (const Affinity& rhs) { if (&rhs!=this) { activeCPUs = rhs.activeCPUs; } return *this; }
+
+
+    /** add a specified cpu core from the list to have affinity to. */
+    void add(unsigned int cpuNmber) { activeCPUs.insert(cpuNmber); }
+
+    /** remove a specified cpu core from the list to have affinity to. */
+    void remove(unsigned int cpuNmber) { activeCPUs.erase(cpuNmber); }
+
+    /** return true if affinity has been provided for specific CPU cores.*/
+    operator bool () const { return !activeCPUs.empty(); }
+
+    typedef std::set<unsigned int> ActiveCPUs;
+
+    /** Set of CPUs that a thread should have affinity to.*/
+    ActiveCPUs activeCPUs;
+};
+
+}
+
+
 // OSGFILE include/osg/Export
 
 //#include<osg/Config>
