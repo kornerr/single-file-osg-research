ef6e67a1b9dc59e8b0c09cd854dc96eccbda6d76
Michael Kapelko <kornerr@gmail.com>
2019-02-14 12:04:57 +0300

    Add osg/Observer

--- OpenSceneGraph.cpp
+++ OpenSceneGraph.cpp
@@ -314,6 +314,82 @@ void Referenced::deleteUsingDeleteHandler() const
 } // end of namespace osg
 
 
+// OSGFILE src/osg/Observer.cpp
+
+//#include <osg/ObserverNodePath>
+//#include <osg/Notify>
+
+using namespace osg;
+
+Observer::Observer()
+{
+}
+
+Observer::~Observer()
+{
+}
+
+ObserverSet::ObserverSet(const Referenced* observedObject):
+    _observedObject(const_cast<Referenced*>(observedObject))
+{
+    //OSG_NOTICE<<"ObserverSet::ObserverSet() "<<this<<std::endl;
+}
+
+ObserverSet::~ObserverSet()
+{
+    //OSG_NOTICE<<"ObserverSet::~ObserverSet() "<<this<<", _observers.size()="<<_observers.size()<<std::endl;
+}
+
+void ObserverSet::addObserver(Observer* observer)
+{
+    //OSG_NOTICE<<"ObserverSet::addObserver("<<observer<<") "<<this<<std::endl;
+    OpenThreads::ScopedLock<OpenThreads::Mutex> lock(_mutex);
+    _observers.insert(observer);
+}
+
+void ObserverSet::removeObserver(Observer* observer)
+{
+    //OSG_NOTICE<<"ObserverSet::removeObserver("<<observer<<") "<<this<<std::endl;
+    OpenThreads::ScopedLock<OpenThreads::Mutex> lock(_mutex);
+    _observers.erase(observer);
+}
+
+Referenced* ObserverSet::addRefLock()
+{
+    OpenThreads::ScopedLock<OpenThreads::Mutex> lock(_mutex);
+
+    if (!_observedObject) return 0;
+
+    int refCount = _observedObject->ref();
+    if (refCount == 1)
+    {
+        // The object is in the process of being deleted, but our
+        // objectDeleted() method hasn't been run yet (and we're
+        // blocking it -- and the final destruction -- with our lock).
+        _observedObject->unref_nodelete();
+        return 0;
+    }
+
+    return _observedObject;
+}
+
+void ObserverSet::signalObjectDeleted(void* ptr)
+{
+    OpenThreads::ScopedLock<OpenThreads::Mutex> lock(_mutex);
+
+    for(Observers::iterator itr = _observers.begin();
+        itr != _observers.end();
+        ++itr)
+    {
+        (*itr)->objectDeleted(ptr);
+    }
+    _observers.clear();
+
+    // reset the observed object so that we know that it's now detached.
+    _observedObject = 0;
+}
+
+
 // OSGFILE src/osg/Notify.cpp
 
 /*
--- OpenSceneGraph.h
+++ OpenSceneGraph.h
@@ -983,6 +983,69 @@ ref_ptr<T> const_pointer_cast(const ref_ptr<Y>& rp) { return const_cast<T*>(rp.g
 }
 
 
+// OSGFILE include/osg/Observer
+
+//#include <OpenThreads/Mutex>
+//#include <osg/Referenced>
+#include <set>
+
+namespace osg {
+
+/** Observer base class for tracking when objects are unreferenced (their reference count goes to 0) and are being deleted.*/
+class OSG_EXPORT Observer
+{
+    public:
+        Observer();
+        virtual ~Observer();
+
+        /** objectDeleted is called when the observed object is about to be deleted.  The observer will be automatically
+        * removed from the observed object's observer set so there is no need for the objectDeleted implementation
+        * to call removeObserver() on the observed object. */
+        virtual void objectDeleted(void*) {}
+
+};
+
+/** Class used by osg::Referenced to track the observers associated with it.*/
+class OSG_EXPORT ObserverSet : public osg::Referenced
+{
+    public:
+
+        ObserverSet(const Referenced* observedObject);
+
+        Referenced* getObserverdObject() { return _observedObject; }
+        const Referenced* getObserverdObject() const { return _observedObject; }
+
+        /** "Lock" a Referenced object i.e., protect it from being deleted
+          *  by incrementing its reference count.
+          *
+          * returns null if object doesn't exist anymore. */
+        Referenced* addRefLock();
+
+        inline OpenThreads::Mutex* getObserverSetMutex() const { return &_mutex; }
+
+        void addObserver(Observer* observer);
+        void removeObserver(Observer* observer);
+
+        void signalObjectDeleted(void* ptr);
+
+        typedef std::set<Observer*> Observers;
+        Observers& getObservers() { return _observers; }
+        const Observers& getObservers() const { return _observers; }
+
+    protected:
+
+        ObserverSet(const ObserverSet& rhs): osg::Referenced(rhs) {}
+        ObserverSet& operator = (const ObserverSet& /*rhs*/) { return *this; }
+        virtual ~ObserverSet();
+
+        mutable OpenThreads::Mutex      _mutex;
+        Referenced*                     _observedObject;
+        Observers                       _observers;
+};
+
+}
+
+
 // OSGFILE include/osg/observer_ptr
 
 /*
@@ -1096,7 +1159,12 @@ public:
     bool operator == (const observer_ptr& wp) const { return _reference == wp._reference; }
     bool operator != (const observer_ptr& wp) const { return _reference != wp._reference; }
     bool operator < (const observer_ptr& wp) const { return _reference < wp._reference; }
+    /*
+     * OPERATOR>
+     * NOTE Somehow `reference`s do not have operator> for the below to work
+     * NOTE Fix later if necessary
     bool operator > (const observer_ptr& wp) const { return _reference > wp._reference; }
+    */
 
     // Non-strict interface, for compatibility
     // comparison operator for const T*.
